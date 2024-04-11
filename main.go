1a0a627fb54a462d0ab68a3b227ff22952260e1e
Son Vo <vnsonvo@gmail.com>
2024-04-12 00:23:04 +0700

    call apiserver from main func

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/vnsonvo/ecom-rest-api/cmd/api"
+)
 
 func main() {
-	fmt.Println("hello world")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file", err)
+	}
+
+	port := os.Getenv("PORT")
+
+	server := api.NewAPIServer(port, nil)
+
+	if err := server.Run(); err != nil {
+		log.Fatal(err)
+	}
 }
