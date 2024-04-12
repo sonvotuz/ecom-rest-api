a0e018fbc7d6635937c8ca1d780ca40726c2c077
Son Vo <vnsonvo@gmail.com>
2024-04-12 10:03:00 +0700

    check user with email, and create create user calling create user func, and write response

--- services/user/routes.go
+++ services/user/routes.go
@@ -9,11 +9,11 @@ import (
 )
 
 type Handler struct {
-	store *types.UserStore
+	store types.UserStore
 }
 
-func NewHandler() *Handler {
-	return &Handler{}
+func NewHandler(store types.UserStore) *Handler {
+	return &Handler{store: store}
 }
 
 func (h *Handler) RegisterRoutes(mux *http.ServeMux, prefixPath string) {
@@ -34,6 +34,24 @@ func (h *Handler) handleRegister(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// check if user exists
+	_, err := h.store.GetUserByEmail(payload.Email)
+	if err == nil {
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("User with email %s already exists", payload.Email))
+		return
+	}
 
 	// if it doenst => create new user
+	err = h.store.CreateUser(types.User{
+		FirstName: payload.FirstName,
+		LastName:  payload.LastName,
+		Email:     payload.Email,
+		// Password:  payload.Password,
+	})
+
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	utils.WriteJSON(w, http.StatusCreated, nil)
 }
