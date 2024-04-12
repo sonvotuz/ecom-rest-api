c197fb2b01b55ca15aecbf9a875fb3949f3fad25
Son Vo <vnsonvo@gmail.com>
2024-04-13 00:53:58 +0700

    helper func to calculate total price and check the availability of stock of items in cart

--- services/cart/service.go
+++ services/cart/service.go
@@ -17,3 +17,32 @@ func getCartItemIds(items []types.CartItem) ([]int, error) {
 
 	return productIds, nil
 }
+
+func checkCartIsInStock(items []types.CartItem, products map[int]types.Product) error {
+	if len(items) == 0 {
+		return fmt.Errorf("Cart is empty")
+	}
+
+	for _, item := range items {
+		product, ok := products[item.ProductID]
+		if !ok {
+			return fmt.Errorf("Product %d is not available in the store, please check your cart", item.ProductID)
+		}
+
+		if product.Quantity < item.Quantity {
+			return fmt.Errorf("Product %s is not available with the quantity requested", product.Name)
+		}
+	}
+
+	return nil
+}
+
+func calculateTotalPrice(items []types.CartItem, productMap map[int]types.Product) float64 {
+	var total float64
+
+	for _, item := range items {
+		product := productMap[item.ProductID]
+		total += product.Price * float64(item.Quantity)
+	}
+	return total
+}
