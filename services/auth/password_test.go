15ac7f694fb33ce1a8be8dab35fa5dbfd2926dc6
Son Vo <vnsonvo@gmail.com>
2024-04-12 16:41:07 +0700

    password hash and compare test

---
+++ services/auth/password_test.go
@@ -0,0 +1,33 @@
+package auth
+
+import "testing"
+
+func TestHashPassword(t *testing.T) {
+	hash, err := HashPassword("password")
+	if err != nil {
+		t.Errorf("error hashing password: %v", err)
+	}
+
+	if hash == "" {
+		t.Error("expected hash to be not empty")
+	}
+
+	if hash == "password" {
+		t.Error("expected hash to be different from password")
+	}
+}
+
+func TestComparePasswords(t *testing.T) {
+	hash, err := HashPassword("password")
+	if err != nil {
+		t.Errorf("error hashing password: %v", err)
+	}
+
+	if !ComparePasswords(hash, "password") {
+		t.Errorf("expected password to match hash")
+	}
+
+	if ComparePasswords(hash, "notpassword") {
+		t.Errorf("expected password to not match hash")
+	}
+}
