0b529bd6401b4db446f957d42860f007ed0ea8da
Son Vo <vnsonvo@gmail.com>
2024-04-12 16:04:47 +0700

    add logic for create user and get user by id

--- services/user/store.go
+++ services/user/store.go
@@ -16,10 +16,12 @@ func NewStore(db *sql.DB) *Store {
 }
 
 func (s *Store) GetUserByEmail(email string) (*types.User, error) {
-	rows, err := s.db.Query("SELECT * FROM users WHERE email = ?", email)
+	rows, err := s.db.Query("SELECT * FROM users WHERE email = $1", email)
+
 	if err != nil {
 		return nil, err
 	}
+
 	user := &types.User{}
 	for rows.Next() {
 		user, err = scanRowIntoUser(rows)
@@ -45,8 +47,29 @@ func scanRowIntoUser(rows *sql.Rows) (*types.User, error) {
 }
 
 func (s *Store) GetUserByID(id int) (*types.User, error) {
-	return nil, nil
+	rows, err := s.db.Query("SELECT * FROM users WHERE email = $1", id)
+	if err != nil {
+		return nil, err
+	}
+	user := &types.User{}
+	for rows.Next() {
+		user, err = scanRowIntoUser(rows)
+		if err != nil {
+			return nil, err
+		}
+	}
+	if user.ID == 0 {
+		return nil, fmt.Errorf("User not found")
+	}
+
+	return user, nil
 }
-func (s *Store) CreateUser(types.User) error {
+
+func (s *Store) CreateUser(user types.User) error {
+	_, err := s.db.Exec("INSERT INTO users (first_name, last_name, email, password) VALUES ($1, $2, $3, $4)", user.FirstName, user.LastName, user.Email, user.Password)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
