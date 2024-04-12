babfc61486c07e589c235118d5e2215dcf9a2dbc
Son Vo <vnsonvo@gmail.com>
2024-04-13 00:50:28 +0700

    add types for order and order item

--- types/types.go
+++ types/types.go
@@ -32,8 +32,10 @@ type LoginUserPayload struct {
 }
 
 type ProductStore interface {
-	GetProducts() ([]*Product, error)
+	GetProducts() ([]Product, error)
 	CreateProduct(CreateProductPayload) error
+	GetProductsById([]int) ([]Product, error)
+	UpdateProduct(Product) error
 }
 
 type Product struct {
@@ -53,3 +55,27 @@ type CreateProductPayload struct {
 	Price       float64 `json:"price" validate:"required"`
 	Quantity    int     `json:"quantity" validate:"required"`
 }
+
+type OrderStore interface {
+	CreateOrder(Order) (int, error)
+	CreateOrderItem(OrderItem) error
+}
+
+type Order struct {
+	ID        int       `json:"id"`
+	UserID    int       `json:"user_id"`
+	Total     float64   `json:"total"`
+	Status    string    `json:"status"`
+	Address   string    `json:"address"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+type OrderItem struct {
+	ID        int       `json:"id"`
+	OrderID   int       `json:"order_id"`
+	ProductID int       `json:"product_id"`
+	Quantity  int       `json:"quantity"`
+	Price     float64   `json:"price"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
