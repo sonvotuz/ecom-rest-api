9702c523addc59108fb33e9cc3b549bb5180d10a
Son Vo <vnsonvo@gmail.com>
2024-04-12 11:16:04 +0700

    add validate tag to register user payload struct

--- types/types.go
+++ types/types.go
@@ -5,10 +5,10 @@ import (
 )
 
 type RegisterUserPayload struct {
-	FirstName string `json:"firstName"`
-	LastName  string `json:"lastName"`
-	Email     string `json:"email"`
-	Password  string `json:"password"`
+	FirstName string `json:"firstName" validate:"required"`
+	LastName  string `json:"lastName" validate:"required"`
+	Email     string `json:"email" validate:"required,email"`
+	Password  string `json:"password" validate:"required,min=3,max=30"`
 }
 
 type User struct {
