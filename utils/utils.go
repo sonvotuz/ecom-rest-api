10ebfae1cc6c19883fa5368826f70eac91e09e3a
Son Vo <vnsonvo@gmail.com>
2024-04-12 09:40:21 +0700

    add utils func to help with encode decode and write response

---
+++ utils/utils.go
@@ -0,0 +1,25 @@
+package utils
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func ParseJSON(r *http.Request, payload any) error {
+	if r.Body == nil {
+		return fmt.Errorf("Missing request body")
+	}
+
+	return json.NewDecoder(r.Body).Decode(&payload)
+}
+
+func WriteJSON(w http.ResponseWriter, status int, val any) error {
+	w.Header().Add("Content-Type", "application/json")
+	w.WriteHeader(status)
+	return json.NewEncoder(w).Encode(val)
+}
+
+func WriteError(w http.ResponseWriter, status int, err error) {
+	WriteJSON(w, status, map[string]string{"error": err.Error()})
+}
