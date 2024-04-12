f061a8762b04153f1d02f8daa4e5f004bf1a803b
Son Vo <vnsonvo@gmail.com>
2024-04-13 02:04:55 +0700

    test few cases of handle checkout

--- services/cart/routes_test.go
+++ services/cart/routes_test.go
@@ -17,6 +17,124 @@ var mockProducts = []types.Product{
 	{ID: 5, Name: "last stock", Price: 30, Quantity: 1},
 }
 
+func TestCartServiceHandler(t *testing.T) {
+	productStore := &mockProductStore{}
+	orderStore := &mockOrderStore{}
+	handler := NewHandler(orderStore, productStore, nil)
+
+	t.Run("should fail to checkout if the cart items do not exist", func(t *testing.T) {
+		payload := types.CartCheckoutPayload{
+			Items: []types.CartItem{
+				{ProductID: 6, Quantity: 16},
+			},
+		}
+
+		marshalled, err := json.Marshal(payload)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		req, err := http.NewRequest(http.MethodPost, "/cart/checkout", bytes.NewBuffer(marshalled))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		responseRecorder := httptest.NewRecorder()
+		mux := http.NewServeMux()
+
+		mux.HandleFunc("POST /cart/checkout", handler.handleCheckout)
+		mux.ServeHTTP(responseRecorder, req)
+
+		if responseRecorder.Code != http.StatusBadRequest {
+			t.Errorf("expected status code %d, got %d", http.StatusBadRequest, responseRecorder.Code)
+		}
+	})
+
+	t.Run("should fail to checkout if the cart has invalid quantity", func(t *testing.T) {
+		payload := types.CartCheckoutPayload{
+			Items: []types.CartItem{
+				{ProductID: 1, Quantity: 0},
+			},
+		}
+
+		marshalled, err := json.Marshal(payload)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		req, err := http.NewRequest(http.MethodPost, "/cart/checkout", bytes.NewBuffer(marshalled))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		responseRecorder := httptest.NewRecorder()
+		mux := http.NewServeMux()
+
+		mux.HandleFunc("POST /cart/checkout", handler.handleCheckout)
+		mux.ServeHTTP(responseRecorder, req)
+
+		if responseRecorder.Code != http.StatusBadRequest {
+			t.Errorf("expected status code %d, got %d", http.StatusBadRequest, responseRecorder.Code)
+		}
+	})
+
+	t.Run("should fail to checkout if there is no stock for an item", func(t *testing.T) {
+		payload := types.CartCheckoutPayload{
+			Items: []types.CartItem{
+				{ProductID: 4, Quantity: 4},
+			},
+		}
+
+		marshalled, err := json.Marshal(payload)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		req, err := http.NewRequest(http.MethodPost, "/cart/checkout", bytes.NewBuffer(marshalled))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		responseRecorder := httptest.NewRecorder()
+		mux := http.NewServeMux()
+
+		mux.HandleFunc("POST /cart/checkout", handler.handleCheckout)
+		mux.ServeHTTP(responseRecorder, req)
+
+		if responseRecorder.Code != http.StatusBadRequest {
+			t.Errorf("expected status code %d, got %d", http.StatusBadRequest, responseRecorder.Code)
+		}
+	})
+
+	t.Run("should fail to checkout if there is not enough stock", func(t *testing.T) {
+		payload := types.CartCheckoutPayload{
+			Items: []types.CartItem{
+				{ProductID: 5, Quantity: 5},
+			},
+		}
+
+		marshalled, err := json.Marshal(payload)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		req, err := http.NewRequest(http.MethodPost, "/cart/checkout", bytes.NewBuffer(marshalled))
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		responseRecorder := httptest.NewRecorder()
+		mux := http.NewServeMux()
+
+		mux.HandleFunc("POST /cart/checkout", handler.handleCheckout)
+		mux.ServeHTTP(responseRecorder, req)
+
+		if responseRecorder.Code != http.StatusBadRequest {
+			t.Errorf("expected status code %d, got %d", http.StatusBadRequest, responseRecorder.Code)
+		}
+	})
+}
+
 type mockProductStore struct{}
 
 func (m *mockProductStore) GetProductByID(productID int) (*types.Product, error) {
