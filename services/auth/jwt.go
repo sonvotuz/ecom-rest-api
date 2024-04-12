86e24d812770d342d9d214c033ba4afc601c765d
Son Vo <vnsonvo@gmail.com>
2024-04-12 16:39:08 +0700

    add func to create jwt token

---
+++ services/auth/jwt.go
@@ -0,0 +1,30 @@
+package auth
+
+import (
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func CreateJWT(secret []byte, userId int) (string, error) {
+	expireTimeStr := os.Getenv("JWTEXPIREINSECONDS")
+	expireTime, err := strconv.Atoi(expireTimeStr)
+	if err != nil {
+		return "", err
+	}
+
+	expiration := time.Second * time.Duration(expireTime)
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"userId":    strconv.Itoa(userId),
+		"expiredAt": time.Now().Add(expiration).Unix(),
+	})
+
+	tokenString, err := token.SignedString(secret)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
