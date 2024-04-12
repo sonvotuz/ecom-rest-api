6ef340fe547a021100c3ee9062a32243e80251ae
Son Vo <vnsonvo@gmail.com>
2024-04-12 09:55:07 +0700

    add get user by email func for store struct

---
+++ services/user/store.go
@@ -0,0 +1,52 @@
+package user
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/vnsonvo/ecom-rest-api/types"
+)
+
+type Store struct {
+	db *sql.DB
+}
+
+func NewStore(db *sql.DB) *Store {
+	return &Store{db: db}
+}
+
+func (s *Store) GetUserByEmail(email string) (*types.User, error) {
+	rows, err := s.db.Query("SELECT * FROM users WHERE email = ?", email)
+	if err != nil {
+		return nil, err
+	}
+	user := &types.User{}
+	for rows.Next() {
+		user, err = scanRowIntoUser(rows)
+		if err != nil {
+			return nil, err
+		}
+	}
+	if user.ID == 0 {
+		return nil, fmt.Errorf("User not found")
+	}
+
+	return user, nil
+}
+
+func scanRowIntoUser(rows *sql.Rows) (*types.User, error) {
+	user := types.User{}
+	err := rows.Scan(&user.ID, &user.FirstName, &user.LastName, &user.Email, &user.Password, &user.CreatedAt)
+	if err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func (s *Store) GetUserByID(id int) (*types.User, error) {
+	return nil, nil
+}
+func (s *Store) CreateUser(types.User) error {
+	return nil
+}
