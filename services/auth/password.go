dfe248a2f89c51d9706fec773816dc334e978d49
Son Vo <vnsonvo@gmail.com>
2024-04-12 10:13:53 +0700

    add hash password func

---
+++ services/auth/password.go
@@ -0,0 +1,12 @@
+package auth
+
+import "golang.org/x/crypto/bcrypt"
+
+func HashPassword(password string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+
+	return string(hash), nil
+}
