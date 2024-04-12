a3ef2e36aeb8e0a3986578f03487bd220f8d07da
Son Vo <vnsonvo@gmail.com>
2024-04-12 17:24:49 +0700

    add create product payload struct and method for interface

--- types/types.go
+++ types/types.go
@@ -33,6 +33,7 @@ type LoginUserPayload struct {
 
 type ProductStore interface {
 	GetProducts() ([]*Product, error)
+	CreateProduct(CreateProductPayload) error
 }
 
 type Product struct {
@@ -43,4 +44,12 @@ type Product struct {
 	Price       float64   `json:"price"`
 	Quantity    int       `json:"quantity"`
 	CreatedAt   time.Time `json:"created_at"`
+}
+
+type CreateProductPayload struct {
+	Name        string  `json:"name" validate:"required"`
+	Description string  `json:"description"`
+	Image       string  `json:"image"`
+	Price       float64 `json:"price" validate:"required"`
+	Quantity    int     `json:"quantity" validate:"required"`
 }
